c6cd3e6a0ac813404221715dcce39e68ceabbd25
Jakub Gocławski <goclawskij@student.mini.pw.edu.pl>
2015-11-02 13:18:55 +0100

    Move sources.

---
+++ src/Automatha/LanguageProcessor/Alphabet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageProcessor
+{
+    public class Alphabet
+    {
+        private char[] letters;
+
+        public List<int> Translate(string s)
+        {
+            List<int> L = s.Select(Translate).ToList();
+            return L;
+        }
+
+        private int Translate(char c)
+        {
+            for(int i=0;i<letters.Length;i++) if (letters[i] == c) return i;
+            return -1;
+        }
+        public string Translate(List<int> L)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var index in L)
+            {
+                sb.Append(letters[index]);
+            }
+            return sb.ToString();
+        }
+        public Alphabet(char[] Set)
+        {
+            letters = Set.ToArray();
+        }
+
+        public char[] Letters
+        {
+            get
+            {
+                return letters;
+            }
+        }
+
+        public char this[int k]
+        {
+            get { return letters[k]; }
+        }
+        
+    }
+}
---
+++ src/Automatha/LanguageProcessor/Machine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageProcessor
+{
+    public class Machine
+    {
+        public double[,] stateFunction;
+        public readonly int States;
+
+        public Alphabet alphabet;
+
+        public int GetFinishedState(string s)
+        {
+            return GetFinishedState(alphabet.Translate(s));
+        }
+
+        public int GetFinishedState(List<int> Word)
+        {
+            int state = 0;
+            foreach (var L in Word)
+            {
+                double c = stateFunction[state, L];
+                c = Math.Round(c);
+                state = (int)c;
+            }
+            return state;
+        }
+
+        private Machine(Alphabet A, double[,] SF, int states)
+        {
+            alphabet = A;
+            stateFunction = SF;
+            States = states;
+        }
+        public static Machine RandomMachineFactory(int states, Alphabet A)
+        {
+            double[,] SF = new double[states, A.Letters.Length];
+            Random R = new Random();
+            for (int i = 0; i < states; i++)
+            {
+                for (int j = 0; j < A.Letters.Length; j++)
+                {
+                    SF[i,j] =(double) R.Next(states);
+                }
+            }
+            return new Machine(A,SF,states);
+        }
+
+        public List<Machine> GetMachinesWithMoreStates(int number)
+        {
+            List<Machine> result = new List<Machine>();
+            for(int i=0;i<number;i++) result.Add(RandomMachineFactory(States+1,alphabet));
+            foreach (var M in result)
+            {
+                for (int i = 0; i < States; i++)
+                {
+                    for (int j = 0; j < alphabet.Letters.Length; j++)
+                    {
+                        M.stateFunction[i, j] = stateFunction[i, j];
+                    }
+                }
+            }
+            return result;
+        }
+        public bool AreWordsInRelation(List<int> Word1, List<int> Word2)
+        {
+            return GetFinishedState(Word1) == GetFinishedState(Word2);
+        }
+
+        public bool AreWordsInRelation(string W1, string W2)
+        {
+            return AreWordsInRelation(alphabet.Translate(W1), alphabet.Translate(W2));
+        }
+
+        public double[,] GetFunctionCopy()
+        {
+            double[,] D = new double[States,alphabet.Letters.Length];
+            for(int i=0;i<States;i++) for (int j = 0; j < alphabet.Letters.Length; j++) D[i, j] = stateFunction[i, j];
+            return D;
+        }
+    }
+}
